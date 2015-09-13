93438cfdef134e16c75f64be7a017d3bc0ca1918
Robert Wright <robert@42itgroup.com>
2015-09-13 12:17:29 -0400

    add chap_3_code_ex

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chap3CodeEx::Application.config.secret_token = 'cd046e2db6f7a118ab4a8a4f7b779365b119bb1cc161e32438b805128f90409c831abb4af7ae9cfcab3d7ee4de0249071b3908a1a28bfcab2af8cbafd61d7a1d'
